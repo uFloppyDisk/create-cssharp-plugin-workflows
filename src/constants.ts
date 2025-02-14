8bcdb6a82cb2c33b86bd6fb7b4bd82854bc3128b
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-02-13 20:32:27 -0800

    refactor: export IS_PRODUCTION constant;

--- src/constants.ts
+++ src/constants.ts
@@ -3,11 +3,10 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
+export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const ROOT = path.join(path.dirname(__filename), "../");
 
-export const TARGET_BASE = process.env.NODE_ENV === 'production'
-  ? process.cwd()
-  : path.join(ROOT, '.playground');
+export const TARGET_BASE = IS_PRODUCTION ? process.cwd() : path.join(ROOT, '.playground');
 
 export const TEMPLATE_BASE = path.join(ROOT, 'templates');
 
